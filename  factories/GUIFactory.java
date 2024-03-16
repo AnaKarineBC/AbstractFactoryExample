79e4b3e1e783b9e232128b155443abcb53d19a18
AnaKarineBC <99502465+AnaKarineBC@users.noreply.github.com>
2024-03-15 22:24:56 -0300

    first commit

---
+++  factories/GUIFactory.java
@@ -0,0 +1,10 @@
+import refactoring_guru.abstract_factory.example.buttons.Button;
+import refactoring_guru.abstract_factory.example.checkboxes.Checkbox;
+
+/**
+ * Abstract factory knows about all (abstract) product types.
+ */
+public interface GUIFactory {
+    Button createButton();
+    Checkbox createCheckbox();
+}
